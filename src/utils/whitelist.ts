6930ca80085eba6a889d15b1e4a6ad5482b112a1
spreadmycode <spreadmycode@outlook.com>
2021-11-24 03:01:00 -0800

    whitelist added

--- src/utils/whitelist.ts
+++ src/utils/whitelist.ts
@@ -6,4 +6,5 @@ export const WHITELIST_FOR_FREE = [
 export const WHITELIST_FOR_PRES = [
     "Ev8HkGcBM7qomSBfLrY88n9PpEneyoua5KquSUKghAKA",
     "HNHW1hTELFegJQG9hxMWm8Uf6LWgeaNZTa5Sdy8g2wce",
+    "dG8osHSkRGqMYXB1dTa9RCezAjgeRWi4tvrG4kB6QDJ",
 ];
